c40858a2912adf64b1be238ec3fabb1a8417eeea
ic21b028 <ic21b028@technikum-wien.at>
2022-06-24 11:48:05 +0200

    Initial commit

---
+++ src/receiver.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int main(int argc, char **Argv){
+    printf("test receiver");
+}
---
+++ src/sender.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+
+
+
+int main(int argc, char **Argv){
+    printf("test sender");
+}
